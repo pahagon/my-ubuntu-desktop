d384f983193778af789d1eaacbd3ae78a3d1e667
pahagon <paulo.ahagon@gmail.com>
2020-11-27 10:56:15 -0300

    [Emacs] init
    Quando iniciar o emacs ele verifica se os pacotes
    estáo instalados

--- emacs/lisp/ensure-packages-installed.el
+++ emacs/lisp/ensure-packages-installed.el
@@ -1,8 +1,8 @@
-(or (file-exists-p package-user-dir)
-    (package-refresh-contents))
+;(or (file-exists-p package-user-dir)
+;    (package-refresh-contents))
 
 (defun ensure-packages-installed (packages)
-  (package-refresh-contents)
+  ;(package-refresh-contents)
   (mapcar (lambda (p) (unless (package-installed-p p)
                               (package-install     p))) packages))
 
